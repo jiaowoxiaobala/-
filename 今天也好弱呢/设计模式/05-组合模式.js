49d9b3308c488108c3c6fe4542783ff93e12249c
jiaowoxiaobala <415313629@qq.com>
2020-08-16 22:24:52 +0800

    20200816

---
+++ 今天也好弱呢/设计模式/05-组合模式.js
@@ -0,0 +1,67 @@
+// 概念: 将对象组合成树型结构来表现整体-部分的层次结构,让使用者可以以一致的
+// 方式处理组合对象以及部分对象
+// 几个特点:
+// -1.结构呈整体-部分的树形关系,整体部分一般称为组合对象,组合对象下还可以有组合对象和页对象
+// -2.组合对象和叶对象有一致的接口和数据结构,以保证操作一致
+// -3.请求从树的最顶端往下传递,如果当前处理请求的对象是叶对象,叶对象自身会对请求作出相应的处
+// 理;如果当前处理的是组合对象,则遍历其下的子节点(叶对象),将请求继续传递给这些子节点
+
+// 文件夹类
+class Folder {
+  constructor(name) {
+    this.name = name;
+    this.parent = null;
+    this.children = [];
+  }
+  // 在文件夹下增加文件或文件夹
+  add(fileOrFolder) {
+    // 给增加的文件或文件夹设置父对象
+    fileOrFolder.parent = this;
+    this.children.push(fileOrFolder);
+    return this;
+  }
+  // 扫描方法
+  scan() {
+    this.children.forEach(child => child.scan());
+  }
+  remove() {
+    // 根节点或树外的游离节点直接return掉
+    if (!this.parent) return;
+    this.children.forEach((child, index) => {
+      if (child === this) {
+        this.children.splice(index, 1);
+      }
+    });
+  }
+}
+
+// 文件类
+class File {
+  constructor(name) {
+    this.name = name;
+    this.parent = null;
+  }
+  // 文件下增加文件,应报错
+  add() {
+    throw new Error('文件下面不能再添加文件')
+  }
+  // 扫描方法
+  scan() {
+    console.log('开始扫描文件: ' + this.name);
+  }
+  remove() {
+    if (!this.parent) return;
+    this.parent.children.forEach((child, index) => {
+      if (child === this) {
+        this.parent.children.splice(index, 1);
+      }
+    });
+  }
+}
+
+const folder = new Folder();
+const file1 = new File('javaScript设计模式与应用');
+const file2 = new File('javaScript函数式编程');
+folder.add(file1).add(file2);
+file2.remove();
+folder.scan();
---
+++ 今天也好弱呢/设计模式/06-模板方法模式.js
@@ -0,0 +1,61 @@
+// 概念: 父类中定义一组操作算法骨架,而将一些实现步骤延迟到子类中,使得子类可以不改变
+// 父类的算法结构的同时,重新定义算法中的某些实现步骤。模板方法模式的关键是算法步骤的
+// 骨架和具体实现分离
+// 组成:
+// AbstractClass: 抽象父类,把一些共用的方法提取出来,把可变的方法作为抽象类,最重要的是
+// 把算法骨架抽象出来作为模板方法
+// templatMethod: 模板方法,固定了希望执行的算法骨架
+// ConcreteClass: 子类,实现抽象父类中定义的抽象方法,调用继承的模板方法时,将执行模板方
+// 法中定义的算法流程
+
+// 使用场景: 如果知道一个算法所需的关键步骤,而且很明确这些步骤的执行顺序,但是具体的实现是
+// 未知的、灵活的,那么这时候就可以使用模板方法模式来将算法步骤的框架抽象出来
+class Beverage {
+  constructor() {
+    // 如果是new调用的Beverage,则new.target等于Beverage
+    if (new.target === Beverage) {
+      throw new Error('抽象类不能直接实例化');
+    }
+  }
+  // 共用方法
+  boilWater() {
+    console.log('水已煮沸');
+  }
+  // 抽象方法
+  brewDrink() {
+    throw new Error('抽象方法不能调用')
+  }
+  // 共用方法
+  pourCup() {
+    console.log('倒进杯子里');
+  }
+  // 抽象方法
+  addCondiment() {
+    throw new Error('抽象方法不能调用')
+  }
+  // 模板方法
+  init() {
+    this.boilWater();
+    this.brewDrink();
+    this.pourCup();
+    this.addCondiment();
+  }
+}
+
+
+class Coffee extends Beverage {
+  constructor() {
+    super();
+  }
+  // 子类实现抽象方法
+  brewDrink() {
+    console.log('冲泡咖啡');
+  }
+  // 子类实现抽象方法
+  addCondiment() {
+    console.log('加点咖啡伴侣');
+  }
+}
+
+const coffee = new Coffee();
+coffee.init();
