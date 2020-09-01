9cc530db70dc6222b7f80767145d858953cc3dcd
jiaowoxiaobala <415313629@qq.com>
2020-09-01 23:13:03 +0800

    20200901

---
+++ 今天也好弱呢/vue/03-函数式组件.vue
@@ -0,0 +1,4 @@
+在template标签上声明functional即可开启函数式组件
+<template functional>
+
+</template>
---
+++ 今天也好弱呢/vue/04-导航守卫.vue
@@ -0,0 +1,14 @@
+vue-router提供的导航守卫主要是用来监听路由的进入和离开
+<script>
+const router = new VueRouter({})
+// 全局前置守卫
+router.beforeEach((to, from, next) => {})
+// to(Router): 即将要进入的目标路由对象
+// from(Router): 当前导航正要离开的路由
+// next(Function): 调用该方法后,才能进入下一个钩子
+
+
+
+// 组件内的守卫,可以在路由组件内直接定义以下路由导航守卫
+// 
+</script>
