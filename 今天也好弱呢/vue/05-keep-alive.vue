f6a537356f8d118ae0008c2399d91cb79ac574ca
jiaowoxiaobala <415313629@qq.com>
2020-09-02 22:50:07 +0800

    20200902

---
+++ 今天也好弱呢/vue/05-keep-alive.vue
@@ -0,0 +1,18 @@
+keep-alive: 主要用于保留组件状态或避免重新渲染
+
+多用于缓存路由组件
+  -使用keep-alive可以将所有路径匹配到的路由组件都缓存起来,包括路由组件里面
+的组件
+<template>
+  <keep-alive>
+    <router-view />
+  </keep-alive>
+</template>
+
+被keep-alive包裹的组件/路由中,会多出两个生命周期钩子: activated和deactivated
+activated: 在组件第一次渲染时会被调用,之后在每次缓存组件被激活时调用
+deactivated: 在组件被停用(离开路由)时调用
+
+注: 使用了keep-alive后就不会调用beforeDestory和destoryed(被缓存起来了,不会被销毁)
+
+可以通过include和exclude有条件地缓存路由/组件
---
+++ 今天也好弱呢/vue/06-vue的一些技巧.vue
@@ -0,0 +1,24 @@
+watch监听多个变量变化:
+  -将多个变量通过计算属性组合在一起,然后用watch监听这个计算属性
+
+vue动态路由参数
+<script>
+const router = new VueRouter({
+  routes: [{
+    path: '/:id',
+    component: component,
+    // 在路由对象中添加props
+    props: true
+  }]
+});
+// 路由组件中
+export default {
+  props: ['id'],
+  methods: {
+    getParams() {
+      // 可以直接从props获取params
+      return this.id
+    }
+  },
+}
+</script>
